21b6dcc261d63748bf8dc253a647b8bd9d5bcce3
Chen-Yiyang <iycspfa@gmail.com>
2018-10-22 01:52:57 +0800

    Update Participants' Copy.py

--- Participants' Copy.py
+++ Participants' Copy.py
@@ -3,45 +3,90 @@ from random import seed
 from random import randrange
 from csv import reader
 
-# Load a CSV file
-def load_csv(filename):
-    dataset = list()
+# Load the data & format it
+def load_data(filename):
+    # Read from the CSV file
+    dataset = []
     with open(filename, 'r') as file:
         csv_reader = reader(file)
         for row in csv_reader:
             if not row:
                 continue
             dataset.append(row)
+
+    '''The data imported is in the format below:
+        [
+            [x1_1,x1_2,x1_3, ..., x1_60, y1],
+            [x2_1,x2_2,x2_3, ..., x2_60, y2],
+            ...
+            ]
+    Each row is one set of data consisting of 61 items,
+    the first 60 are the input values (i.e. the sonar data),
+    the last one is the actual result of either a 'M' (metal) or 'R' (rock)'''
+
+    # Convert the string of the input values to float
+    # Use 0 & 1 to represent 'M' and 'R' respectively for calculation
+    for row in dataset:
+        for i in range(len(row)-1): # leave out the last one
+            row[i] = float(row[i].strip())
+
+        if row[-1] == 'M':  # metal
+            row[-1] = 0
+        else:   # rock
+            row[-1] = 1
+
     return dataset
 
-# Convert string column to float
-def str_column_to_float(dataset, column):
-    for row in dataset:
-        row[column] = float(row[column].strip())
 
-# Convert string column to integer
-def str_column_to_int(dataset, column):
-    class_values = [row[column] for row in dataset]
-    unique = set(class_values)
-    lookup = dict()
-    for i, value in enumerate(unique):
-        lookup[value] = i
-    for row in dataset:
-        row[column] = lookup[row[column]]
-    return lookup
+# Prediction function
+def predict(row, weights):
+    '''Recall:
+    h(x) = w0 + w1*x0 + w2*x1 + ... (as x is 0-indexed)
 
-# Split a dataset into k folds
-def cross_validation_split(dataset, n_folds):
-    dataset_split = list()
-    dataset_copy = list(dataset)
-    fold_size = int(len(dataset) / n_folds)
-    for i in range(n_folds):
-        fold = list()
-        while len(fold) < fold_size:
-            index = randrange(len(dataset_copy))
-            fold.append(dataset_copy.pop(index))
-        dataset_split.append(fold)
-    return dataset_split
+    If h(x) < 0, result is 0 (metal)
+    If h(x) > 0, result is 1 (rock)
+    '''
+    # Calculate h(x)
+
+    # ReLU
+
+
+# train weights
+def train_weights(train_data, l_rate, n_epoch):
+    # Initialise the weights to zero
+
+
+    # Each epoch is the process of using all the training data for training once.
+    for epoch in range(n_epoch):
+        sum_error = 0.0
+        for row in train_data:
+            
+
+            # Count no. of wrong predictions
+            
+
+            # Update w0
+            # Update w1, w2, ...
+
+        # Show the process
+        # by printing out the no. of errors after every 5 epoches
+        
+    return weights
+
+
+# perceptron model
+def perceptron(train_data, test_data, l_rate, n_epoch):
+    # Train the model
+    weights = train_weights(train_data, l_rate, n_epoch)
+
+    # Make predictions
+    predictions = []
+    for row in test_data:
+        prediction = predict(row, weights)
+        predictions.append(prediction)
+
+    return(predictions)
+
 
 # Calculate accuracy percentage
 def accuracy_metric(actual, predicted):
@@ -51,82 +96,69 @@ def accuracy_metric(actual, predicted):
             correct += 1
     return correct / float(len(actual)) * 100.0
 
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = []
+    dataset_copy = list(dataset) # do not change the original dataset list
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+
 # Evaluate an algorithm using a cross validation split
-def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+def evaluate(dataset, n_folds, l_rate, n_epoch):
     folds = cross_validation_split(dataset, n_folds)
-    scores = list()
+    scores = []
+
+    fold_count = 0
     for fold in folds:
-        train_set = list(folds)
-        train_set.remove(fold)
-        train_set = sum(train_set, [])
-        test_set = list()
+        # each time take all except for one fold for training,
+        # and the remaining one fold for testing
+        train_data = list(folds)
+        train_data.remove(fold)
+        train_data = sum(train_data, [])
+        
+        test_data = []
+        actual = []
         for row in fold:
             row_copy = list(row)
-            test_set.append(row_copy)
+            test_data.append(row_copy)
+
+            actual.append(row_copy[-1])
             row_copy[-1] = None
-        predicted = algorithm(train_set, test_set, *args)
-        actual = [row[-1] for row in fold]
+
+
+        print("Fold no.: %d" % fold_count)
+        fold_count += 1
+
+        # Train and make predictions
+        predicted = perceptron(train_data, test_data, l_rate, n_epoch)
+
+        # Evaluate
         accuracy = accuracy_metric(actual, predicted)
         scores.append(accuracy)
     return scores
 
-# Prediction function
-def predict(row,weights):
-    score = weights[0]
-    for i in range(len(row)-1):
-        ## make prediction
-
-        
-    return 1.0 if score >= 0.0 else 0.0
 
 
-# train weights
-def train_weights(train, l_rate, n_epoch):
-    weights = [0.0 for i in range(len(train[0]))]
-    for epoch in range(n_epoch):
-        sum_error = 0.0
-        for row in train:
-            prediction = predict(row,weights)
-            ## calculate errot
-
-            
-            sum_error += error**2
-            weights[0] = weights[0] + l_rate * error
-            for i in range(len(row)-1):
-                #update weights
-
-                
-                
-        print('Epoch: %d, Error=%.3f' % (epoch,sum_error))
-    return weights
-
-
-# perceptron model
-def perceptron(train, test, l_rate, n_epoch):
-    predictions = list()
-    weights = train_weights(train,l_rate,n_epoch)
-    for row in test:
-        prediction = predict(row,weights)
-        predictions.append(prediction)
-    return(predictions)
-
-
-# Test the Perceptron algorithm on the sonar dataset
-seed(1)
-
-# load and prepare data
+# Load and Prepare data
 filename = 'sonar.all-data.csv'
-dataset = load_csv(filename)
-for i in range(len(dataset[0])-1):
-    str_column_to_float(dataset, i)
-    
-# convert string class to integers
-str_column_to_int(dataset, len(dataset[0])-1)
+dataset = load_data(filename)
 
-# evaluate algorithm
+
+# Define Hyper-parameters
 n_folds = 3
 l_rate = 0.01
 n_epoch = 100
-scores = evaluate_algorithm(dataset, perceptron, n_folds, l_rate, n_epoch)
+
+
+# Evaluate the algorithm
+scores = evaluate(dataset, n_folds, l_rate, n_epoch)
 print('Scores: %s' % scores)
-print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+print('Average Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
